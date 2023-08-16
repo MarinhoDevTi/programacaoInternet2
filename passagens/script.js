523c149c76c4af38f600b997b297b9cc995e6bc8
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2023-08-15 21:48:00 -0300

    Update

---
+++ passagens/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btnOrcamento = document.getElementById("btnOrcamento");
+    btnOrcamento.addEventListener("click", realizarOrcamento);
+});
+
+function realizarOrcamento() {
+    // Código para realizar o orçamento
+}
+
+// Outras funções e manipulações aqui
